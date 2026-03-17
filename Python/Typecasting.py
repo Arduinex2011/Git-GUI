ba75d29bee6f02d7eaa2d806bbb1a3f22a1e6989
Arduinex2011 <adriellomeli11@icloud.com>
2026-03-16 21:37:44 -0500

    Create Typecasting.py

---
+++ Python/Typecasting.py
@@ -0,0 +1,14 @@
+# Typecasting = the process of converting a variable from one data type to another
+#               str(), int(), float(), bool()
+
+name = "Test"
+age = 14
+gpa = 3.2
+is_admin = False
+
+age = str(age)
+print(age)
+
+name = bool(name)
+print(name)
+# Without a name, the result will lead to False
