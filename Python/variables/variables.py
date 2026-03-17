faca9d7be6bc020b7a535689420bf4c4b9a7d05d
Arduinex2011 <adriellomeli11@icloud.com>
2026-03-16 20:57:48 -0500

    Update variables.py

--- Python/variables/variables.py
+++ Python/variables/variables.py
@@ -1,1 +1,16 @@
+# NOTE: This project is NOT from me and is only from BroCode
 
+user_name = "adriell1003"
+print("Hello " + user_name)
+
+year = 2026
+print("Today's date is " + year)
+
+pi = 3.14
+print("Your favorite number is Pi, also known in decimals as " + pi)
+
+is_admin = True
+if is_admin:
+  print(f"Hello, {user_name}. You are an admin")
+else:
+  print(f"Unfortunately, {user_name}. You aren't an admin")
